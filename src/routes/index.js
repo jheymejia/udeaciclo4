9924a8dffa099494135e1924a19b671da22c9911
Yulder A. Avila <77408569+Y-Avila@users.noreply.github.com>
2021-11-18 12:24:41 -0500

    ComenzarMaqueta

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,11 @@ const express=require("express")
 const router=express.Router()
 
 router.get("/", (req,res)=>{
-    res.render("../Home")
+    res.render("./index")
 })
 
 router.get("/aboutus", (req,res)=>{
-    res.send("Esto es Acerca de nosotros ")
+    res.render("./about")
 })
 
 module.exports=router;
